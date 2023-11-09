dfe9cea8581f8ea8120b14fb5f0812329a5e41cc
Tony <AIB924@student.bham.ac.uk>
2023-11-09 17:04:31 +0000

    Adding Solana connect wallet functionality

--- client/App.tsx
+++ client/App.tsx
@@ -2,7 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Routes as Index } from "./routes";
 import { RecoilRoot } from "recoil";
-import { Suspense } from "react";
+import { Suspense, useMemo } from "react";
 import { Admin } from "./routes/admin";
 import { Auth } from "./routes/auth";
 import { SignIn } from "./routes/auth/sign-in";
@@ -10,31 +10,69 @@ import { SignUp } from "./routes/auth/sign-up";
 import { NotFound } from "./routes/not-found";
 import { Vote } from "./routes/vote";
 import { PrivacyPolicy } from "./routes/privacy-policy";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 function App() {
+  // you can use Mainnet, Devnet or Testnet here
+  const solNetwork = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(solNetwork), [solNetwork]);
+  // initialise all the wallets you want to use
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ solNetwork }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+    ],
+    [solNetwork]
+  );
+
   return (
-    <RecoilRoot>
-      {}
-      <Suspense fallback={"loading"}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Auth />}>
-              <Route path="/" element={<SignIn />} />
-            </Route>
-            <Route path={Index.ADMIN()} element={<Admin />} />
-            <Route path={Index.AUTH()} element={<Auth />}>
-              <Route path={Index.AUTH_SIGN_UP()} element={<SignUp />} />
-              <Route path={Index.AUTH_SIGN_IN()} element={<SignIn />} />
-              <Route path={Index.AUTH()} element={<SignIn />} />
-            </Route>
-            <Route path={Index.VOTE()} element={<Vote />} />
-            <Route path={Index.PRIVACY_POLICY()} element={<PrivacyPolicy />} />
-            <Route path={Index.NOT_FOUND()} element={<NotFound />} />
-            <Route path={"*"} element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-    </RecoilRoot>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <div className="App">
+            <RecoilRoot>
+              {}
+              <Suspense fallback={"loading"}>
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<Auth />}>
+                      <Route path="/" element={<SignIn />} />
+                    </Route>
+                    <Route path={Index.ADMIN()} element={<Admin />} />
+                    <Route path={Index.AUTH()} element={<Auth />}>
+                      <Route path={Index.AUTH_SIGN_UP()} element={<SignUp />} />
+                      <Route path={Index.AUTH_SIGN_IN()} element={<SignIn />} />
+                      <Route path={Index.AUTH()} element={<SignIn />} />
+                    </Route>
+                    <Route path={Index.VOTE()} element={<Vote />} />
+                    <Route
+                      path={Index.PRIVACY_POLICY()}
+                      element={<PrivacyPolicy />}
+                    />
+                    <Route path={Index.NOT_FOUND()} element={<NotFound />} />
+                    <Route path={"*"} element={<NotFound />} />
+                  </Routes>
+                </BrowserRouter>
+              </Suspense>
+            </RecoilRoot>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
--- client/routes/auth/sign-up.tsx
+++ client/routes/auth/sign-up.tsx
@@ -2,9 +2,43 @@ import { Helmet } from "react-helmet";
 import { Config } from "../../config";
 import { Link } from "react-router-dom";
 import { Routes } from "../index";
-import { Button, TextInput } from "@carbon/react";
+import { PortInput } from "@carbon/icons-react";
+import { Button, InlineNotification, TextInput } from "@carbon/react";
+import { FormEvent, useRef, useState } from "react";
+import styles from "./style.module.scss";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+type FormError = "password-mismatch" | "duplicate-email" | null;
 
 export const SignUp = () => {
+  const { connection } = useConnection();
+  const { publicKey, connected, sendTransaction } = useWallet();
+
+  const form = useRef<HTMLFormElement>(null);
+  const passwords = [
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+  ];
+  const [error, setError] = useState<FormError>(null);
+  const [formEnabled, setFormEnabled] = useState(true);
+  const submit = async (event: FormEvent) => {
+    event.preventDefault();
+    // Lock form to prevent multiple submissions
+    setFormEnabled(false);
+
+    // Ensure password fields match
+    if (passwords[0].current?.value !== passwords[1].current?.value) {
+      setError("password-mismatch");
+      setFormEnabled(true);
+      return;
+    }
+
+    //TODO Construct JSON payload from form data and send to server
+  };
   return (
     <>
       <Helmet>
@@ -12,15 +46,52 @@ export const SignUp = () => {
       </Helmet>
       <h2>Sign up</h2>
       <p className={"styles logInHint"}>
-        Already have an account?
+        Already have an account?{" "}
         <Link to={Routes.AUTH_SIGN_IN()}>Sign in instead</Link>
       </p>
-      <form>
-        <TextInput id="" labelText="" />
-        <TextInput id="" labelText="" />
-        <TextInput.PasswordInput />
-        <TextInput.PasswordInput />
-        <Button></Button>
+      <form className={styles.form} onSubmit={submit} ref={form}>
+        <div>
+          <WalletMultiButton />
+        </div>
+        <TextInput
+          id="sign-up__email"
+          type="text"
+          pattern={Config.ORG.EMAIL_REGEX.source}
+          labelText="Email"
+          placeholder={Config.ORG.EMAIL_PLACEHOLDER}
+          name="email"
+          invalid={error === "duplicate-email"}
+          required
+        />
+        <TextInput.PasswordInput
+          id="sign-up__password1"
+          labelText="Password"
+          name="password1"
+          ref={passwords[0]}
+          invalid={error === "password-mismatch"}
+          required
+        />
+        <TextInput.PasswordInput
+          id="sign-up__password2"
+          labelText="Confirm password"
+          name="password2"
+          ref={passwords[1]}
+          invalid={error === "password-mismatch"}
+          required
+        />
+        <Button renderIcon={PortInput} type="submit" disabled={!formEnabled}>
+          Sign up
+        </Button>
+        {error && (
+          <InlineNotification
+            title={
+              error == "duplicate-email"
+                ? "An account with this email already exists. Try signing in instead"
+                : "Password fields do not match"
+            }
+            hideCloseButton
+          />
+        )}
       </form>
     </>
   );
