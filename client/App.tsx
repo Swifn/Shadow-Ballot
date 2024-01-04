b367e61faf91ed010adb98d86115c03391092bd8
Tony <AIB924@student.bham.ac.uk>
2024-01-04 16:37:26 +0000

    Removed blockchain aspect and renamed 'Vote' to 'Election' for clarity purposes.

--- client/App.tsx
+++ client/App.tsx
@@ -2,77 +2,46 @@ import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Routes as Index } from "./routes";
 import { RecoilRoot } from "recoil";
-import { Suspense, useMemo } from "react";
+import { Suspense } from "react";
 import { Admin } from "./routes/admin";
 import { Auth } from "./routes/auth";
 import { SignIn } from "./routes/auth/sign-in";
 import { SignUp } from "./routes/auth/sign-up";
 import { Voter } from "./routes/voter";
 import { NotFound } from "./routes/not-found";
-import { Vote } from "./routes/vote";
+import { Election } from "./routes/election";
 import { PrivacyPolicy } from "./routes/privacy-policy";
-import { clusterApiUrl } from "@solana/web3.js";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 function App() {
-  const solNetwork = WalletAdapterNetwork.Mainnet;
-  const endpoint = useMemo(() => clusterApiUrl(solNetwork), [solNetwork]);
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new TorusWalletAdapter(),
-      new LedgerWalletAdapter(),
-    ],
-    [solNetwork]
-  );
-
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
-        <WalletModalProvider>
-          <div className="App">
-            <RecoilRoot>
-              {}
-              <Suspense fallback={"loading"}>
-                <BrowserRouter>
-                  <Routes>
-                    <Route path="/" element={<Auth />}>
-                      <Route path="/" element={<SignIn />} />
-                    </Route>
-                    <Route path={Index.ADMIN()} element={<Admin />} />
-                    <Route path={Index.AUTH()} element={<Auth />}>
-                      <Route path={Index.AUTH_SIGN_UP()} element={<SignUp />} />
-                      <Route path={Index.AUTH_SIGN_IN()} element={<SignIn />} />
-                      <Route path={Index.AUTH()} element={<SignIn />} />
-                    </Route>
-                    <Route path={Index.VOTER()} element={<Voter />} />
-                    <Route path={Index.VOTE()} element={<Vote />} />
-                    <Route
-                      path={Index.PRIVACY_POLICY()}
-                      element={<PrivacyPolicy />}
-                    />
-                    <Route path={Index.NOT_FOUND()} element={<NotFound />} />
-                    <Route path={"*"} element={<NotFound />} />
-                  </Routes>
-                </BrowserRouter>
-              </Suspense>
-            </RecoilRoot>
-          </div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div className="App">
+      <RecoilRoot>
+        {}
+        <Suspense fallback={"loading"}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Auth />}>
+                <Route path="/" element={<SignIn />} />
+              </Route>
+              <Route path={Index.ADMIN()} element={<Admin />} />
+              <Route path={Index.AUTH()} element={<Auth />}>
+                <Route path={Index.AUTH_SIGN_UP()} element={<SignUp />} />
+                <Route path={Index.AUTH_SIGN_IN()} element={<SignIn />} />
+                <Route path={Index.AUTH()} element={<SignIn />} />
+              </Route>
+              <Route path={Index.VOTER()} element={<Voter />} />
+              <Route path={Index.VOTE()} element={<Election />} />
+              <Route
+                path={Index.PRIVACY_POLICY()}
+                element={<PrivacyPolicy />}
+              />
+              <Route path={Index.NOT_FOUND()} element={<NotFound />} />
+              <Route path={"*"} element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </RecoilRoot>
+    </div>
   );
 }
 
