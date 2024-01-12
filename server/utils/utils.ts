ceec58de4609110a3077efcb53dec79d92a07f8d
Tony <AIB924@student.bham.ac.uk>
2024-01-12 18:48:48 +0000

    added a utils file with helper functions for the controllers

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -1,5 +1,9 @@
 import { Society } from "../models/society.model.js";
 import { VoterSociety } from "../models/voter-society.model.js";
+import { Op } from "sequelize";
+import { Election } from "../models/election.model.js";
+import { ElectionCandidates } from "../models/election-candidates.model.js";
+import { Vote } from "../models/vote.model.js";
 
 export const doesSocietyExist = async (societyId: number): Promise<boolean> => {
   const existingSociety = await Society.findOne({
@@ -22,10 +26,25 @@ export const isInSociety = async (
     },
   });
 
-  return inSociety !== null;
+  return inSociety === null;
 };
 
-export const isSocietyOwner = async (societyId, voterId): Promise<boolean> => {
+export const isNameUnique = async (name: string): Promise<boolean> => {
+  const uniqueName = await Society.findOne({
+    where: {
+      name: {
+        [Op.like]: name,
+      },
+    },
+  });
+
+  return uniqueName === null;
+};
+
+export const isSocietyOwner = async (
+  societyId: number,
+  voterId: number
+): Promise<boolean> => {
   const isOwner = await Society.findOne({
     where: {
       societyId: societyId,
@@ -34,4 +53,55 @@ export const isSocietyOwner = async (societyId, voterId): Promise<boolean> => {
   });
 
   return isOwner !== null;
+};
+
+export const doesElectionExist = async (
+  electionId: number
+): Promise<boolean> => {
+  const existingElection = await Election.findOne({
+    where: {
+      electionId: electionId,
+    },
+  });
+
+  return existingElection !== null;
+};
+
+export const isElectionOpen = async (electionId: number): Promise<boolean> => {
+  const open = await Election.findOne({
+    where: {
+      electionId: electionId,
+      electionStatus: 1,
+    },
+  });
+
+  return open === null;
+};
+
+export const isCandidateInElection = async (
+  electionId: number,
+  voterId: number
+): Promise<boolean> => {
+  const validCandidate = await ElectionCandidates.findOne({
+    where: {
+      electionId: electionId,
+      voterId: voterId,
+    },
+  });
+
+  return validCandidate === null;
+};
+
+export const isNewVote = async (
+  voterId: number,
+  electionId: number
+): Promise<boolean> => {
+  const newVote = await Vote.findOne({
+    where: {
+      voteId: voterId,
+      electionId: electionId,
+    },
+  });
+
+  return newVote === null;
 };
