0e7326ef20fd11ba2f6ba7d21079f2053ea68116
Tony <AIB924@student.bham.ac.uk>
2024-01-15 14:12:36 +0000

    Updated models path + added magic numbers

--- server/controller/auth.controller.ts
+++ server/controller/auth.controller.ts
@@ -2,41 +2,49 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { AuthConfig } from "../configs/auth.config.js";
 import { Voter } from "../models/index.js";
+import * as HTTP from "../utils/magicNumbers.js";
 
+//TODO: hash or encrypt voter email addresses in the database to prevent identities being dereferenced
+// remove the REGEX?
 export const signUp = async (req, res) => {
-  if (req.body.password1 != req.body.password2) {
-    return res.status(400).send({ message: "Password mismatch" });
+  const { email, password1, password2 } = req.body;
+  if (password1 != password2) {
+    return res
+      .status(HTTP.STATUS_BAD_REQUEST)
+      .send({ message: "Password mismatch" });
   }
-  const hash = await bcrypt.hash(req.body.password1, AuthConfig.SALT);
+  const hash = await bcrypt.hash(password1, AuthConfig.SALT);
 
   try {
-    const newVoterInstance = {
-      voterId: req.body.voterId,
-      email: req.body.email,
+    const newVoter = {
+      email: email,
       password: hash,
     };
-    // await Voter.create({
-    //   voterId: req.body.voterId,
-    //   email: req.body.email,
-    //   password: hash,
-    // });
-    return res.status(200).send(newVoterInstance);
-  } catch (e) {
-    return res.status(500).send(e);
+    await Voter.create(newVoter);
+    return res
+      .status(HTTP.STATUS_OK)
+      .send({ message: "Sign up was successful" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(HTTP.STATUS_INTERNAL_SERVER_ERROR)
+      .send({ error: "unable to create account" });
   }
 };
 
 export const signIn = async (req, res) => {
+  const { email, password } = req.body;
   const voter = await Voter.findOne({
     where: {
-      voterId: req.body.voterId,
-      email: req.body.email,
+      email: email,
     },
   });
 
-  const auth = await bcrypt.compare(req.body.password, voter?.password ?? "");
+  const auth = await bcrypt.compare(password, voter?.password ?? "");
   if (!auth || !voter) {
-    return res.status(401).send({ message: "Credentials do not match" });
+    return res
+      .status(HTTP.STATUS_UNAUTHORIZED)
+      .send({ message: "Credentials do not match" });
   }
 
   const token = jwt.sign(
@@ -47,7 +55,7 @@ export const signIn = async (req, res) => {
     }
   );
 
-  return res.status(200).send({
+  return res.status(HTTP.STATUS_OK).send({
     id: voter.voterId,
     token,
   });
