645c54c5673bd589219aa37ad2d44a724e8a60cc
Tony <AIB924@student.bham.ac.uk>
2024-01-16 15:22:04 +0000

    setting up protected routes

--- client/state/user-state.ts
+++ client/state/user-state.ts
@@ -1,14 +1,14 @@
 import { atom } from "recoil";
 import { Config } from "../config";
 import { get } from "../utils/fetch";
-import { User } from "./types";
+import { Voter } from "./types";
 
-export const userState = atom<User | null>({
-  key: "USER",
+export const userState = atom<Voter | null>({
+  key: "VOTER",
   default: (async () => {
-    const userId = localStorage.getItem(Config.STORAGE.USER_ID_KEY);
-    if (!userId) return null;
-    const response = await get(`users/${userId}`);
+    const voterId = localStorage.getItem(Config.STORAGE.USER_ID_KEY);
+    if (!voterId) return null;
+    const response = await get(`voter/${voterId}`);
     if (!response.ok) return null;
     const payload = await response.json();
     return payload;
