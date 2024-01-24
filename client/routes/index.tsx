998158ad0f28da35561adad196325f70baeac4c3
Tony <AIB924@student.bham.ac.uk>
2024-01-24 16:12:18 +0000

    Fix: voter routes

--- client/routes/index.tsx
+++ client/routes/index.tsx
@@ -4,8 +4,12 @@ export const Routes = {
   AUTH_SIGN_UP: () => `${Routes.AUTH()}/sign-up`,
   AUTH_SIGN_IN: () => `${Routes.AUTH()}/sign-in`,
   NOT_FOUND: () => `/404`,
-  VOTER: (uid?: string) => `/voter/${uid ?? ":uid"}`,
-  ELECTION: (vid?: string) => `election/${vid ?? ":vid"}`,
+  VOTER: () => `/voter`,
+  ELECTION: (eid?: string) => `election/${eid ?? ":eid"}`,
   PRIVACY_POLICY: () => `/privacy-policy`,
   SOCIETY: () => `/society`,
+  SOCIETY_CREATE: () => `${Routes.SOCIETY()}/create`,
+  SOCIETY_JOIN: () => `${Routes.SOCIETY()}/join`,
+
+  LANDING: () => `/landing`,
 };
