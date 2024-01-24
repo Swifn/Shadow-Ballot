9fa5b917301a9c21e4c2454eaaf5ef4a86f34ee3
Tony <AIB924@student.bham.ac.uk>
2024-01-24 16:02:10 +0000

    Update: card component to allow multiple buttons

--- client/components/cards/index.tsx
+++ client/components/cards/index.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styles from "./style.module.scss";
 import { Button } from "@carbon/react";
 import { PortInput } from "@carbon/icons-react";
+
+interface buttonConfig {
+  label: string;
+  eventHandler: () => void;
+  icon?: React.ElementType;
+  kind: React.ElementType;
+}
 
 export const Cards = ({
   profilePicture = "./client/assets/bg.jpg",
   name,
   description,
-  button,
-  eventHandler,
-  icon,
+  buttons,
 }) => {
   return (
     <div className={styles.card}>
@@ -17,14 +22,20 @@ export const Cards = ({
       <div className={styles.cardContent}>
         <h3>{name}</h3>
         <p>{description}</p>
-        <Button
-          onClick={eventHandler}
-          className={styles.button}
-          renderIcon={icon}
-          type="submit"
-        >
-          {button}
-        </Button>
+        <div className="buttonContainer">
+          {buttons.map((buttonConfig, index) => (
+            <Button
+              key={index}
+              onClick={buttonConfig.eventHandler}
+              className={styles.button}
+              renderIcon={buttonConfig.icon}
+              kind={buttonConfig.kind}
+              type="submit"
+            >
+              {buttonConfig.label}
+            </Button>
+          ))}
+        </div>
       </div>
     </div>
   );
